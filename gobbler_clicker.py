e48a9fa2e0b4089c19ed2464f0714ce30812772f
davkam <david.jr.kamran@gmail.com>
2023-08-26 16:12:43 +0200

    Improvement Updates

--- gobbler_clicker.py
+++ gobbler_clicker.py
@@ -1,31 +1,60 @@
 import keyboard
+import os
 import pyautogui
 import pystray
+import sys
 import threading
+import tkinter
 import time
 import winsound
-from PIL import Image
+from PIL import Image, ImageDraw
 from tkinter import messagebox
+
+def create_icon():
+	image = Image.new('RGBA', (64, 64), (0, 0, 0, 0))
+	image_draw = ImageDraw.Draw(image)
+	image_draw.rounded_rectangle(
+		(8, 8, 56, 56),	# X,Y Coordinates (X0, Y0, X1, Y1)
+		radius=10,
+		fill='white',
+		outline='black'
+	)
+	return image
 
 def on_start(loop_event: threading.Event):
 	def _on_start(icon: pystray.Icon, item: pystray.MenuItem):
 		loop_event.set()
-		winsound.Beep(1000, 150)
+		winsound.MessageBeep()
 	return _on_start
 
 def on_stop(loop_event: threading.Event):
 	def _on_stop(icon: pystray.Icon, item: pystray.MenuItem):
 		loop_event.clear()
-		winsound.Beep(800, 150)
+		winsound.MessageBeep()
 	return _on_stop
 
 def on_about():
 	def _on_about(icon: pystray.Icon, item: pystray.MenuItem):
-		about_message = "Gobbler Clicker\n"
-		about_message += "A simple AFK autoclicker for the Candy Corn Gobbler.\n"
-		about_message += "Press F9 to start the autoclicker while the mousepointer is on the gobbler,"
-		about_message += " end the autoclicker by pressing F10."
-		messagebox.showinfo("About", about_message)
+		about_message = "Gobbler Clicker v1.0\n\n"
+		about_message += "A simple AFK autoclicker for the Candy Corn Gobbler in GW2.\n"
+		about_message += "The clicker simulates mouse doubleclicks every 5 seconds.\n"
+		about_message += "To start the autoclicker press F9, to end it press F10.\n"
+		about_message += "Keep mousepointer over gobbler while autoclicker is active.\n\n"
+
+		root = tkinter.Tk()
+		root.title(" About Gobbler Clicker v1.0")
+		root.geometry("600x300")
+
+		if os.path.exists("icon/gobbler_clicker_icon.ico"):
+			root.iconbitmap("icon/gobbler_clicker_icon.ico")
+		
+		label = tkinter.Label(root, text=about_message, wraplength=450)
+		label.pack(padx=20, pady=20)
+
+		ok_button = tkinter.Button(root, text="OK", command=root.destroy, width=10)
+		ok_button.pack()
+
+		root.mainloop()
 	return _on_about
 
 def on_exit(quit_event: threading.Event, loop_event: threading.Event):
@@ -36,7 +65,12 @@ def on_exit(quit_event: threading.Event, loop_event: threading.Event):
 	return _on_exit
 
 def run_tray_icon(quit_event: threading.Event, loop_event: threading.Event):
-	image = Image.open("icon/gobbler_clicker_icon.png")
+	if os.path.exists("icon/gobbler_clicker_icon.ico"):
+		image = Image.open("icon/gobbler_clicker_icon.ico")
+	else:
+		messagebox.showerror(title="[ERROR] File Not Found", message="[ERROR] File 'icon/gobbler_clicker_icon.ico' could not be found.\nGeneric icon used instead.")
+		image = create_icon()
+	
 	menu = (
 		pystray.MenuItem("Start (F9)", on_start(loop_event=loop_event)),
 		pystray.MenuItem("Stop (F10)", on_stop(loop_event=loop_event)),
