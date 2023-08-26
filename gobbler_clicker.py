e3d26f550d121e4ad6f718072623b5e93a2d704f
davkam <david.jr.kamran@gmail.com>
2023-08-26 06:41:03 +0200

    Added About-MessageBox and Activation/Deactivation Sounds

--- gobbler_clicker.py
+++ gobbler_clicker.py
@@ -3,21 +3,29 @@ import pyautogui
 import pystray
 import threading
 import time
+import winsound
 from PIL import Image
+from tkinter import messagebox
 
 def on_start(loop_event: threading.Event):
 	def _on_start(icon: pystray.Icon, item: pystray.MenuItem):
 		loop_event.set()
+		winsound.Beep(1000, 150)
 	return _on_start
 
 def on_stop(loop_event: threading.Event):
 	def _on_stop(icon: pystray.Icon, item: pystray.MenuItem):
 		loop_event.clear()
+		winsound.Beep(800, 150)
 	return _on_stop
 
 def on_about():
 	def _on_about(icon: pystray.Icon, item: pystray.MenuItem):
-		pass
+		about_message = "Gobbler Clicker\n"
+		about_message += "A simple AFK autoclicker for the Candy Corn Gobbler.\n"
+		about_message += "Press F9 to start the autoclicker while the mousepointer is on the gobbler,"
+		about_message += " end the autoclicker by pressing F10."
+		messagebox.showinfo("About", about_message)
 	return _on_about
 
 def on_exit(quit_event: threading.Event, loop_event: threading.Event):
@@ -32,7 +40,7 @@ def run_tray_icon(quit_event: threading.Event, loop_event: threading.Event):
 	menu = (
 		pystray.MenuItem("Start (F9)", on_start(loop_event=loop_event)),
 		pystray.MenuItem("Stop (F10)", on_stop(loop_event=loop_event)),
-		pystray.MenuItem("About", on_about),
+		pystray.MenuItem("About", on_about()),
 		pystray.MenuItem("Exit", on_exit(quit_event=quit_event, loop_event=loop_event))
 	)
 	icon = pystray.Icon("Gobbler Clicker Icon", image, "Gobbler Clicker", menu)
